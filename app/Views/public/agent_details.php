bcdd78f9bef5fce4fc1a418aa4a8c4149bd60a99
Amal Ganesh <amalganesh4u@gmail.com>
2024-07-23 11:14:42 +0530

    Agent details

--- app/Views/public/agent_details.php
+++ app/Views/public/agent_details.php
@@ -18,11 +18,11 @@
   <div class="container">
     <div class="row align-items-center">
       <div class="col-md-5 col-lg-7 order-md-1 text-center text-md-start z-index-2 cta-image">
-        <img class="img-fluid mb-4 mb-md-0" src="<?php echo base_url('assets/');?>brand_partner_screens/dashboard_new.png" width="850" alt="" />
+        <img class="img-fluid mb-4 mb-md-0" src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" width="850" alt="" />
       </div>
       <div class="col-md-7 col-lg-5 text-center text-md-start">
-        <h1 class="display-3 fw-bold lh-sm">Introducing Aranea's Brand Partner Program</h1>
-        <p class="my-4">The Brand Partner Program is designed for individuals and organizations looking to collaborate on healthcare products. As a brand partner, you'll have the opportunity to invest in a wide range of healthcare solutions, manage your portfolio through an intuitive dashboard, and earn returns based on product sales. Leverage our market insights to make informed investment decisions and drive profitability.</p>
+        <h1 class="display-3 fw-bold lh-sm">Introducing Aranea's Agent Program</h1>
+        <p class="my-4">The Agent Program is designed for individuals and organizations looking to collaborate on healthcare products. As an agent, you'll have the opportunity to invest in a wide range of healthcare solutions, manage your portfolio through an intuitive dashboard, and earn returns based on product sales. Leverage our market insights to make informed investment decisions and drive profitability.</p>
         <div class="d-flex justify-content-center d-md-inline-block">
           <a class="btn btn-primary me-2" href="sign-in-url">Sign In</a>
           <a class="btn btn-secondary" href="register-url">Register</a>
@@ -32,11 +32,10 @@
   </div>
 </section>
 
-
 <section class="py-8" id="testimonial">
   <div class="container-lg">
     <div class="row flex-center">
-      <img class="img-fluid mb-4" src="<?php echo base_url('assets/');?>brand_partner_screens/products.png" alt="" style="max-width: 300px;" />
+      <img class="img-fluid mb-4" src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard_mobile.png" alt="" style="max-width: 300px;" />
       <div class="col-sm-10 col-md-6 col-lg-6 text-center text-md-start offset-md-1">
         <h6 class="fw-bold fs-4 display-3 lh-sm">Explore a Wide Variety of Products on Our Platform</h6>
         <p class="my-4 pe-xl-5">The rise of mobile devices transforms the way we consume. Elevate your experience with our diverse product offerings.</p>
@@ -46,7 +45,7 @@
               <div class="row h-100">
                 <div class="col-12">
                   <!-- Product image -->
-                  <img src="<?php echo base_url('assets/');?>brand_partner_screens/product_details.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
+                  <img src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
                 </div>
                 <div class="col-xl-6 pt-4 d-flex d-sm-block flex-center">
                   <div class="d-flex justify-content-center d-md-inline-block mt-4">
@@ -61,7 +60,7 @@
               <div class="row h-100">
                 <div class="col-12">
                   <!-- Product image -->
-                  <img src="<?php echo base_url('assets/');?>brand_partner_screens/product_details.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
+                  <img src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
                 </div>
                 <div class="col-xl-6 pt-4 d-flex d-sm-block flex-center">
                   <div class="d-flex justify-content-center d-md-inline-block mt-4">
@@ -76,13 +75,13 @@
               <div class="row h-100">
                 <div class="col-12">
                   <!-- Product image -->
-                  <img src="<?php echo base_url('assets/');?>brand_partner_screens/product_details.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
+                  <img src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" class="img-fluid" alt="Product Details" style="max-width: 300px;">
                 </div>
                 <div class="col-xl-6 pt-4 d-flex d-sm-block flex-center">
                   <div class="d-flex justify-content-center d-md-inline-block mt-4">
                     <h5 class="fw-bold lh-sm">Profitable Investment Opportunities</h5>
                   </div>
-                  <p class="text-center text-md-start">Join our Brand Partner Program and invest in products that promise high profitability and market growth.</p>
+                  <p class="text-center text-md-start">Join our Agent Program and invest in products that promise high profitability and market growth.</p>
                 </div>
               </div>
             </div>
@@ -101,19 +100,15 @@
   </div>
 </section>
 
-            <!-- <section> close ============================-->
-      <!-- ============================================-->
-
-
-      
-
+<!-- <section> close ============================-->
 <!-- ============================================-->
+
 <section class="py-5" id="features">
   <div class="container-lg">
     <div class="row align-items-center">
       <div class="col-md-7 col-lg-6 px-sm-5 px-md-0 order-md-0">
         <h6 class="fw-bold fs-4 display-3 lh-sm">Manage Your Portfolio with Ease</h6>
-        <p class="my-4">As a brand partner, you gain access to a comprehensive portfolio management system. Monitor your product holdings, track performance, and stay updated with the latest market trends.</p>
+        <p class="my-4">As an agent, you gain access to a comprehensive portfolio management system. Monitor your product holdings, track performance, and stay updated with the latest market trends.</p>
         <div class="d-flex align-items-center mb-5">
           <div class="px-0">
             <h5 class="fw-bold text-danger">Detailed Insights</h5>
@@ -126,16 +121,13 @@
             <p>Track the real-time status of your product holdings and make informed decisions based on the latest market data.</p>
           </div>
         </div>
-
       </div>
       <div class="col-md-5 col-lg-6 order-md-1 text-center text-md-start">
-        <img class="img-fluid shadow-effect" src="<?php echo base_url('assets/');?>brand_partner_screens/portfolio_web.png" width="550" alt="Brand Partner Dashboard" />
+        <img class="img-fluid shadow-effect" src="<?php echo base_url('assets/');?>agent_screens/agent_portfolio.jpg" width="550" alt="Agent Dashboard" />
       </div>
     </div>
   </div>
 </section>
-
-
 
 <style>
   .shadow-effect {
@@ -147,68 +139,61 @@
   <div class="container">
     <div class="row align-items-center">
       <div class="col-md-5 order-md-0 text-center text-md-start">
-        <img class="img-fluid mb-4" src="<?php echo base_url('assets/');?>brand_partner_screens/market.png" width="550" alt="Market Insights" />
+        <img class="img-fluid mb-4" src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" width="550" alt="Market Insights" />
       </div>
       <div class="col-md-6 text-center text-md-start offset-md-1">
         <h6 class="fw-bold fs-4 display-3 lh-sm">Track Market Trends<br />with Precision</h6>
         <p class="my-4 pe-xl-5">Stay informed about the market value of all our products with Aranea's comprehensive platform. Monitor price changes and market trends effortlessly, ensuring you have the latest insights to make informed decisions.</p>
-        <a class="btn btn-lg btn-primary rounded-pill hover-top" href="<?php echo base_url('brand_partner_registration') ?>" role="button">Explore Market Trends</a>
+        <a class="btn btn-lg btn-primary rounded-pill hover-top" href="<?php echo base_url('agent_registration') ?>" role="button">Explore Market Trends</a>
       </div>
     </div>
   </div>
 </section>
 
-
 <section class="py-6">
   <div class="container">
     <div class="row justify-content-center mb-6">
       <div class="col-lg-6 text-center mx-auto mb-3 mb-md-5 mt-4">
-        <h6 class="fw-bold fs-4 display-3 lh-sm">Why Choose <br />Aranea's Brand Partner Program</h6>
-        <p class="mb-0">Experience the power of Aranea's platform, designed to elevate your investments in healthcare products. Here's why Aranea stands out:</p>
+        <h6 class="fw-bold fs-4 display-3 lh-sm">Why Choose <br />Aranea's Agent Program</h6>
+        <p class="mb-0">Experience the power of Aranea's platform, designed to elevate your investments in the healthcare sector.</p>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/app.png" width="90" alt="Intuitive Interface" />
-          <h5 class="fw-bold">Intuitive Interface</h5>
-          <p class="mb-md-0">Navigate seamlessly through our platform with an intuitive user interface, ensuring a hassle-free experience.</p>
+    <div class="row flex-center">
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="text-center">
+          <img class="img-fluid mb-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRefn20MzFwN-lyanIEz3dw6V2h1TYihihkQg&s" width="150" alt="Investment Opportunities" />
+          <h6 class="fw-bold fs-4 lh-sm">Lucrative Investment Opportunities</h6>
+          <p>Discover and invest in high-demand healthcare products with substantial growth potential.</p>
         </div>
       </div>
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/time-award.png" width="90" alt="Reliable Performance" />
-          <h5 class="fw-bold">Reliable Performance</h5>
-          <p class="mb-md-0">Count on Aranea for reliable performance, ensuring consistent and dependable service to meet your healthcare investment needs.</p>
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="text-center">
+          <img class="img-fluid mb-4" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSO69II3JzrrQGjMjAZbWNHQWJ9xmccjBL3fA&s" width="150" alt="Market Insights" />
+          <h6 class="fw-bold fs-4 lh-sm">Comprehensive Market Insights</h6>
+          <p>Leverage detailed market insights to make informed investment decisions and maximize your returns.</p>
         </div>
       </div>
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/cloud.png" width="90" alt="Secure Cloud Storage" />
-          <h5 class="fw-bold">Secure Cloud Storage</h5>
-          <p class="mb-md-0">Rest assured with our secure cloud storage solutions, safeguarding your data with the highest levels of encryption.</p>
+      <div class="col-lg-4 col-md-6 mb-4">
+        <div class="text-center">
+          <img class="img-fluid mb-4" src="https://cdn-icons-png.flaticon.com/512/12285/12285915.png" width="150" alt="Portfolio Management" />
+          <h6 class="fw-bold fs-4 lh-sm">Robust Portfolio Management</h6>
+          <p>Manage your investments with our user-friendly portfolio management tools, designed to simplify your experience.</p>
         </div>
       </div>
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/customization.png" width="90" alt="Tailored Solutions" />
-          <h5 class="fw-bold">Tailored Solutions</h5>
-          <p class="mb-md-0">Customize your experience with Aranea's tailored solutions, ensuring that your investments meet your unique needs and requirements.</p>
-        </div>
+    </div>
+  </div>
+</section>
+
+<section class="py-6">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-5 order-md-0 text-center text-md-start">
+        <img class="img-fluid mb-4" src="<?php echo base_url('assets/');?>agent_screens/agent_dashboard.png" width="550" alt="Contact Us" />
       </div>
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/ux.png" width="90" alt="User Experience Design" />
-          <h5 class="fw-bold">User Experience Design</h5>
-          <p class="mb-md-0">Benefit from Aranea's user-centric design approach, ensuring a seamless and intuitive user experience across all aspects of our platform.</p>
-        </div>
-      </div>
-      <div class="col-md-4 mb-6">
-        <div class="text-center px-lg-3">
-          <img class="img-fluid mb-3" src="<?php echo base_url('assets/');?>assets/img/illustrations/support.png" width="90" alt="Customer Support" />
-          <h5 class="fw-bold">Customer Support</h5>
-          <p class="mb-md-0">Receive dedicated support from Aranea's team of experts, ensuring that you have the assistance you need every step of the way.</p>
-        </div>
+      <div class="col-md-6 text-center text-md-start offset-md-1">
+        <h6 class="fw-bold fs-4 display-3 lh-sm">Join Aranea's Agent Program Today!</h6>
+        <p class="my-4 pe-xl-5">Don't miss out on this exciting opportunity to invest in the healthcare industry. Join Aranea's Agent Program today and start your journey towards profitable investments.</p>
+        <a class="btn btn-lg btn-primary rounded-pill hover-top" href="<?php echo base_url('agent_registration') ?>" role="button">Get Started Now</a>
       </div>
     </div>
   </div>
