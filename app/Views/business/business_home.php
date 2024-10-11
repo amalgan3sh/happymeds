2ba2cd4a9434986aae2e9809b33f11db538fc88b
Amal Ganesh <amalganesh4u@gmail.com>
2024-10-11 12:21:00 +0530

    spyderhub

--- app/Views/business/business_home.php
+++ app/Views/business/business_home.php
@@ -369,8 +369,8 @@
         ***********************************-->
        <div class="footer">
 			<div class="copyright">
-				<p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/"
-						target="_blank">DexignLab</a> <span class="current-year">2024</span>
+				<p>Copyright © Designed &amp; Developed by <a href="#"
+						target="_blank">SpyderHub</a> <span class="current-year">2024</span>
 				</p>
 			</div>
 		</div>
