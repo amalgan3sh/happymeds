2290a5a5bf2e72c000c59e0525cf0c5e1f879599
Amal Ganesh <amalganesh4u@gmail.com>
2024-11-08 10:42:55 +0530

    spelling

--- app/Views/public/partnership.php
+++ app/Views/public/partnership.php
@@ -6,7 +6,7 @@
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="breadcrumb-inner text-center">
-                                <h3 class="title h3">Partnershir</h3>
+                                <h3 class="title h3">Partnership</h3>
                                 <ul class="page-list">
                                     <li class="rainbow-breadcrumb-item"><a href="<?php echo base_url('/') ?>">Home</a></li>
                                     <li class="rainbow-breadcrumb-item active">Partnership</li>
